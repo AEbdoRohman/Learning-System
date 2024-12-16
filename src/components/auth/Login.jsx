d99f5f47f53a3ba3ca57652288eaf81b07aa5ade
MohamedBadrr <badr59497@gmail.com>
2024-12-16 11:53:29 +0200

    all translations with all rights directions

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <section className="fixed top-0 left-0  w-full h-[100vh]  bg-black bg-opacity-50 z-[100] mt-[0px] flex items-center justify-center">
       <div className="p-4 w-[500px] mx-[20px] h-[513px] bg-white rounded-[30px]">
-        <div className="flex justify-between">
+        <div className="flex  justify-between">
           <h1 className="text-[20px] font-bold">{t("auth.login")}</h1>
           <span className="text-[20px] font-bold">
             <Link to={"/"}>x</Link>
@@ -13,25 +13,25 @@ const Login = () => {
         </div>
         <form action="">
           <div className="flex flex-col w-full mt-[20px]">
-            <label htmlFor="email" className="block text-right mb-[8px] ">
+            <label htmlFor="email" className="block  mb-[8px] ">
               {t("auth.email")}
             </label>
             <input
               required
               type="email"
-              className="px-[10px] text-right border-gray-400 border-[1px] w-full h-[40px] rounded-[10px] bg-gray-100 focus:border-[#525fe1] focus:outline-none"
+              className="px-[10px]  border-gray-400 border-[1px] w-full h-[40px] rounded-[10px] bg-gray-100 focus:border-[#525fe1] focus:outline-none"
               id="email"
             />
           </div>
-          <div className="flex flex-col w-full mt-[5px]">
-            <label htmlFor="password" className="block text-right mb-[8px] ">
+          <div className="flex flex-col w-full mt-[5px] ">
+            <label htmlFor="password" className="block  mb-[8px] ">
               {t("auth.password")}
             </label>
             <input
               required
               type="password"
               placeholder="••••••••"
-              className="border-gray-400 px-[10px] text-right border-[1px] w-full h-[40px] rounded-[10px] bg-gray-100 focus:border-[#525fe1] focus:outline-none"
+              className="border-gray-400 px-[10px]  border-[1px] w-full h-[40px] rounded-[10px] bg-gray-100 focus:border-[#525fe1] focus:outline-none"
               id="password"
             />
           </div>
